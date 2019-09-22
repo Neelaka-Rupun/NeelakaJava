0227df09bce3fbfde1cdd9e71b7a03d3f401d03a
Neelaka-Rupun <rnelaka@gmail.com>
2019-09-22 20:47:37 +0530

    third commit of Mortage calcutalion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,38 +8,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-         final int  MONTH_IN_YEARS = 12;
-         final int  PERCENT = 100;
+         final int  MONTH_IN_YEARS  = 12;
+         final int  PERCENT         = 100;
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please enter the Principle :");
-        String principle = scanner.nextLine().trim();
+        String principle               = scanner.nextLine().trim();
 
         System.out.print("Annual Interest Rate :");
-        String  intrestRate = scanner.nextLine().trim();
+        String  intrestRate         = scanner.nextLine().trim();
 
         System.out.print("Period (Years) :");
-        String  period = scanner.nextLine().trim();
+        String  period              = scanner.nextLine().trim();
 
 
-        double intPrinciple                = Double.parseDouble(principle);
-        double intMontthlyIntrasRate    = (Double.parseDouble(intrestRate)/MONTH_IN_YEARS)/PERCENT;
+        double intPrinciple                 = Double.parseDouble(principle);
+        double intMontthlyIntrasRate        = (Double.parseDouble(intrestRate)/MONTH_IN_YEARS)/PERCENT;
 
 
-        int numberOfPayments = Integer.parseInt(period) * MONTH_IN_YEARS ;
+        int numberOfPayments                = Integer.parseInt(period) * MONTH_IN_YEARS ;
 
-        double value = 1 + intMontthlyIntrasRate;
+        double value                        = 1 + intMontthlyIntrasRate;
 
-        double numerator = intMontthlyIntrasRate*(Math.pow( value,(double)numberOfPayments));
+        double numerator                    = intMontthlyIntrasRate*(Math.pow( value,(double)numberOfPayments));
 
-        double denominator = (Math.pow(value,(double)numberOfPayments))-1;
+        double denominator                  = (Math.pow(value,(double)numberOfPayments))-1;
 
         double valueTobeMultiplyByPrinciple = numerator/denominator;
 
-        double monatage = intPrinciple * valueTobeMultiplyByPrinciple;
+        double monatage                     = intPrinciple * valueTobeMultiplyByPrinciple;
 
-        String currencyMonatage = NumberFormat.getCurrencyInstance().format(monatage);
+        String currencyMonatage             = NumberFormat.getCurrencyInstance().format(monatage);
 
         System.out.print("Montage :" + currencyMonatage);
     }
